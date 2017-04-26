2af42f279f567bc183f0ed05d7e26221314e673d
Chris.Dumrique <dumrique7@gmail.com>
2017-04-26 13:34:45 -0500

    adding dto

--- YoungGuns/WebApi/Controllers/FieldController.cs
+++ YoungGuns/WebApi/Controllers/FieldController.cs
@@ -11,33 +11,22 @@ namespace WebApi.Controllers
     [Route("api/[controller]")]
     public class FieldController : Controller
     {
-        // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        public string Get()
         {
             return "value";
         }
-
-        // POST api/values
+        
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]string form_id, [FromBody]string form_name)
         {
         }
-
-        // PUT api/values/5
+        
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
-        // DELETE api/values/5
+        
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
