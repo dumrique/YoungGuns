4c676d7c19acdfcc68a927f13de694226d640497
Zeb.Hardy <Zeb.Hardy@wolterskluwer.com>
2017-04-26 13:37:15 -0500

    more stuff

---
+++ YoungGuns/YoungGuns.Data/Contracts/AdjacencyListItem.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoungGuns.Data.Contracts
+{
+    public class AdjacencyListItem : TableEntity
+    {
+        public List<uint> DependentFields { get; set; }
+    }
+}
--- YoungGuns/YoungGuns.Data/ReturnChangeset.cs
+++ YoungGuns/YoungGuns.Data/ReturnChangeset.cs
@@ -8,10 +8,15 @@ namespace YoungGuns.Data
         /// User Id of the user pushing this changeset
         /// </summary>
         public string Owner { get; set; }
+        
+        /// <summary>
+        /// Return Id for the return
+        /// </summary>
+        public string ReturnId { get; set; }
         /// <summary>
         /// Version of the return DAG on which changeset is based
         /// </summary>
-        public uint BaseVersion { get; set; }
+        public int BaseVersion { get; set; }
 
         /// <summary>
         /// Changeset values to be updated (key: field ID, value: new value)
