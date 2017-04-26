ca8d44d104cba71b43cbdcbc85e1c16bcd8a4387
McKinney, Matt <William.McKinney@wolterskluwer.com>
2017-04-26 13:56:57 -0500

    adding form submission service

--- YoungGuns.UI/app/scripts/services/form-submit-service.js
+++ YoungGuns.UI/app/scripts/services/form-submit-service.js
@@ -4,6 +4,13 @@ angularApp.service('FormSubmitService', function FormSubmitService($http) {
 
     return {
         submit: function(form) {
+            const baseurl = 'youngguns.southcentralus.cloudapp.azure.com:19000';
+            
+            for(var i=0; i < form.form_fields.length; i++) {
+                var field = form.form_fields[i];       
+                field.type = field.field_calculation ? 'calc' : 'info';
+            }
+
             console.log(form);
         }
     };
