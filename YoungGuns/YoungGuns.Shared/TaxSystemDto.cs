0c85d8a3ff3e0c942b4f937b2d02d8275acb24c1
Eric.Jensen <eric.jensen@wolterskluwer.com>
2017-04-26 15:42:59 -0500

    Rename TaxSystemDto properties

--- YoungGuns/YoungGuns.Shared/TaxSystemDto.cs
+++ YoungGuns/YoungGuns.Shared/TaxSystemDto.cs
@@ -8,9 +8,9 @@ namespace YoungGuns.Shared
 {
     public class TaxSystemDto
     {
-        public string form_id;
-        public string form_name;
-        public List<FieldDto> form_fields;
+        public string taxsystem_id;
+        public string taxsystem_name;
+        public List<FieldDto> taxsystem_fields;
         public bool submitted;
     }
 }
