a8cbba9c8ae135662b12c15019048430bd9a2c96
Chris.Dumrique <dumrique7@gmail.com>
2017-04-26 16:48:22 -0500

    Starting DAS

---
+++ YoungGuns/YoungGuns.DataAccess/DbHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoungGuns.Data;
+using YoungGuns.Shared;
+
+namespace YoungGuns.DataAccess
+{
+    public class DbHelper
+    {
+        public TaxSystem GetTaxSystem(string id)
+        {
+            return null;
+        }
+
+        public List<TaxSystem> GetAllTaxSystems()
+        {
+            return null;
+        }
+
+        public void InsertTaxSystem(TaxSystem system)
+        {
+            
+        }
+
+        public CalcDAG GetCalcDag(string taxSystemId)
+        {
+            return null;
+        }
+
+        public void InsertCalcDag(CalcDAG dag)
+        {
+            
+        }
+
+        public string GetEndpoint
+    }
+}
