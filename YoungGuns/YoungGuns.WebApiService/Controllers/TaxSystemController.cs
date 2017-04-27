d76971ceba4f90cea35ce440cdcf83eda7faddd8
Chris.Dumrique <dumrique7@gmail.com>
2017-04-26 20:41:41 -0500

    SF WIP

---
+++ YoungGuns/YoungGuns.WebApiService/Controllers/TaxSystemController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using System.Threading.Tasks;
+using System.Web.Http;
+using YoungGuns.Business;
+using YoungGuns.Data;
+using YoungGuns.DataAccess;
+using YoungGuns.Shared;
+using YoungGuns.WebApi.Map;
+
+namespace YoungGuns.WebApi.Controllers
+{
+    [Route("api/taxsystem")]
+    public class TaxSystemController : ApiController
+    {
+        private readonly IMapper _map;
+        private readonly DbHelper _dbHelper;
+
+        public TaxSystemController()
+        {
+            _map = AutomapperConfig.Create();
+            _dbHelper = new DbHelper();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var taxSystems = _dbHelper.GetAllTaxSystems();
+
+            return Ok(taxSystems);
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody]PostTaxSystemRequest request)
+        {
+            var taxSystem = _map.Map<TaxSystem>(request);
+
+            var id = await _dbHelper.UpsertTaxSystem(taxSystem);
+            await AdjacencyListBuilder.ExtractAndStoreAdjacencyList(request);
+            
+            return Ok(id);
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> Put(string id, [FromBody]PostTaxSystemRequest request)
+        {
+            var taxSystem = _map.Map<TaxSystem>(request);
+            taxSystem.Id = id;
+            await _dbHelper.UpsertTaxSystem(taxSystem);
+
+            await DAGUtilities.DeleteAdjacencyListTable(taxSystem.Name);
+            await AdjacencyListBuilder.ExtractAndStoreAdjacencyList(request);
+
+            return Ok();
+        }
+        
+    }
+}
---
+++ YoungGuns/YoungGuns.WebApiService/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Owin;
+
+namespace YoungGuns.WebApiService
+{
+    public class Startup : IOwinAppBuilder
+    {
+        public static void ConfigureFormatters(MediaTypeFormatterCollection formatters)
+        {
+            formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+            ConfigureFormatters(config.Formatters);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
